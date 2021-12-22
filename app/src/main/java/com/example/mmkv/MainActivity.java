0f0a1f7940cb41c70aa6d8b19f65aa273286c80d
yyg <58300663+yyg001@users.noreply.github.com>
2021-12-22 16:32:35 +0800

    1

---
+++ app/src/main/java/com/example/mmkv/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mmkv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.tencent.mmkv.MMKV;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.tv);TextView textView2=findViewById(R.id.tv2);
+        EditText editText=findViewById(R.id.et);
+        Button button1=findViewById(R.id.btn1);
+        Button button2=findViewById(R.id.btn2);
+        String rootDir = MMKV.initialize(this);
+        System.out.println("mmkv root: " + rootDir);
+        // 获取默认的全局实例
+        MMKV kv = MMKV.defaultMMKV();
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+                kv.encode("1",editText.getText().toString());
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.setText(kv.decodeString("1"));
+            }
+        });
+
+
+
+
+
+
+//// 根据业务区别存储, 附带一个自己的 ID
+//        MMKV kv = MMKV.mmkvWithID("MyID");
+//
+//// 多进程同步支持
+//        MMKV kv = MMKV.mmkvWithID("MyID", MMKV.MULTI_PROCESS_MODE);
+
+
+    }
+}
